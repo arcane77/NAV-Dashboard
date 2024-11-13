eb92360a97b4591309ba66eb13de69aa4e1a37dd
ANJALI K S <anjaliks2446@gmail.com>
2024-11-13 22:20:21 +0530

    comm

--- src/components/Dashboard/ChartSection.js
+++ src/components/Dashboard/ChartSection.js
@@ -26,7 +26,7 @@ const ChartSection = ({ data }) => {
   }, []);
 
   return (
-    <div className="h-48 sm:h-64 md:h-96 md:mb-6 w-full -ml-10">
+    <div className="h-48 sm:h-64 md:h-96 md:mb-6 w-120% -ml-10">
       <ResponsiveContainer width="100%" height="100%">
         <LineChart data={data} margin={{ top: 20, right: 30, left: 60, bottom: 20 }}>
           <CartesianGrid strokeDasharray="3 3" />
